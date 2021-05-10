fc281361e9f4fdbb64a7ae8ba8579a4173743491
zyrif <rubaiyat9@gmail.com>
2021-05-10 06:20:09 +0600

    wip

---
+++ src/components/Search.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-container fill-height>
+        <v-row
+            align="self-end"
+            dense
+        >
+            <v-col>
+                <v-text-field
+                    label="Youtube Playlist URL" 
+                    outlined
+                ></v-text-field>
+            </v-col>
+        </v-row>
+        <v-row
+            align="self-start"
+            dense
+        >
+            <v-spacer></v-spacer>
+            <v-col
+                style="text-align: center"
+            >
+                <v-btn
+                    outlined 
+                >
+                    Check
+                </v-btn>
+            </v-col>
+            <v-spacer></v-spacer>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ src/views/Landing.vue
@@ -0,0 +1,15 @@
+<template>
+    <search>
+    </search>
+</template>
+
+<script>
+
+import Search from '@/components/Search.vue'
+
+export default {
+    components: {
+        Search
+    }
+}
+</script>
