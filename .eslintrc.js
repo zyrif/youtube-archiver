2c2751d21553b11d057a66c6303c401202c28d2e
zyrif <rubaiyat9@gmail.com>
2021-08-31 00:37:12 +0600

    Misc: Add jest and package.json to Git

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,38 @@
 module.exports = {
   root: true,
+
   env: {
     node: true
   },
+
   'extends': [
     'plugin:vue/essential',
     'eslint:recommended'
   ],
+
   'plugins': [
     'vuetify'
   ],
+
   parserOptions: {
     parser: 'babel-eslint'
   },
+
   rules: {
     'vuetify/no-deprecated-classes': 'warn',
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+  },
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 }
---
+++ tests/unit/search.spec.js
@@ -0,0 +1,19 @@
+// import Search from '@/components/Search.vue'
+
+describe('Playlist URL Autocomplete Hints', () => {
+    it('shows protocol autocomplete hint', () => {
+        // like h should show ttps:// 
+    })
+
+    it('shows subdomain autocomplete hint', () => {
+        // like w for www. and m for m.
+    })
+
+    it('shows domain autocomplete hint', () => {
+        // like y for youtube.com
+    })
+
+    it('shows path autocomplete hint', () => {
+        // like p for playlist?list=
+    })
+})
