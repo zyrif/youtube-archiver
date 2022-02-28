3f90182864dc711c0486715396926fdc0d64e809
zyrif <rubaiyat9@gmail.com>
2022-03-01 02:44:00 +0600

    Remove mock data, show real data from API

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -12,55 +12,35 @@
 import DashboardList from '../components/DashboardList.vue';
 export default {
   components: { DashboardList },
+  mounted: function () {
+    if (this.playlists.length < 1) {
+      this.$axios
+        .get('/playlists/')
+        .then((response) => {
+          if (response.status === 200) {
+            if (Array.isArray(response.data)) {
+              this.playlists = response.data
+            } else {
+              // TODO: Better error alerting
+              console.error("Expected array from from /playlists/ api response data")
+              console.log(response.data)
+            }
+          }
+        })
+        .catch((error) => {
+          console.error('Error while fetching playlists');
+          console.log(error);
+        });
+    }
+  },
   data: () => ({
-    playlists: [
-      {
-        id: 'PLAB27A3C12C31E663',
-        title: 'Inertia',
-        channel: 'Veritasium',
-        numOfVideos: 8,
-        numOfVideosTracked: 8,
-        lastChecked: new Date('2021-06-21'),
-      },
-      {
-        id: 'PL772556F1EFC4D01C',
-        title: 'Misconceptions',
-        channel: 'Veritasium',
-        numOfVideos: 33,
-        numOfVideosTracked: 33,
-        lastChecked: new Date('2021-06-21'),
-      },
-      {
-        id: 'FLHnyfMqiRRG1u-2MsSQLbXA',
-        title: 'Favourites',
-        channel: 'Veritasium',
-        numOfVideos: 26,
-        numOfVideosTracked: 26,
-        lastChecked: new Date('2021-06-21'),
-      },
-      {
-        id: 'PL67A3EFF8267A8DF9',
-        title: 'Radiation',
-        channel: 'Veritasium',
-        numOfVideos: 5,
-        numOfVideosTracked: 5,
-        lastChecked: new Date('2021-06-21'),
-      },
-      {
-        id: 'PLAC555EC7D3D0F4AA',
-        title: 'Science Experiments!',
-        channel: 'Veritasium',
-        numOfVideos: 19,
-        numOfVideosTracked: 19,
-        lastChecked: new Date('2021-06-21'),
-      },
-    ],
+    playlists: [],
   }),
   computed: {
     breadcrumbs: function () {
       return this.$route.meta.breadCrumbs;
-    }
-  }
+    },
+  },
 };
 </script>
 
