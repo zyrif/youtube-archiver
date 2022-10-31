0869a48e212c72304e4767967439e5f82a2abcd2
zyrif <rubaiyat9@gmail.com>
2022-10-31 13:26:01 +0600

    Chore: Add eslint rule to ignore single word component names

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,6 +21,7 @@ module.exports = {
     "vuetify/no-deprecated-classes": "warn",
     "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "vue/multi-word-component-names": "off",
   },
 
   overrides: [
